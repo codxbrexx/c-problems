31ae835ff2dda048115025c92a2e6c60f0cf5318
mozammilali01 <mozammilali2018@gmail.com>
2024-08-02 00:44:06 +0530

    Add files via upload

---
+++ problem_5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{ int num;
+  printf("Enter a number : ");
+  scanf("%d", &num);
+  printf("The int value is :%d\n",num);
+
+
+  float num2;
+  printf("Enter a number : ");
+  scanf("%f", &num2);
+  printf("The float value is :%f\n",num2);
+
+  double num3;
+  printf("Enter a number : ");
+  scanf("%d", &num3);
+  printf("The double value is :%d",num3);
+
+  return 0;
+}
