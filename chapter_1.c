db245370c5d634fbfbed97352e6666f90f8d64ed
mozammilali01 <mozammilali2018@gmail.com>
2024-08-02 00:42:48 +0530

    Add files via upload

---
+++ chapter_1.c
@@ -0,0 +1,19 @@
+
+// task 1
+#include<stdio.h>
+
+int main()
+  {
+    printf("*\n* *\n* * *\n* * * *\n* * * * *\n\n");
+    printf("* * * * *\n* * * *\n* * *\n* *\n*\n\n");
+    printf("         *\n       * *\n     * * *\n   * * * *\n * * * * *\n\n");
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* * * *\n");
+    printf("* * * * *\n");
+
+
+
+    return 0;
+  }
