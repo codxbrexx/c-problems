7506b6c78f1dc5b1f8d1c926a5170fd9e6b2c347
mozammilali01 <mozammilali2018@gmail.com>
2024-08-02 00:43:32 +0530

    Add files via upload

---
+++ problem_4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{  int val;
+   printf("Enter 1st value : ");
+   scanf("%d",&val);
+   printf("first value is :%d \n",val);
+
+  printf("Enter 2nd value : ");
+  scanf("%d",&val);
+  printf("Second value is :%d \n",val);
+
+  return 0;
+}
